44af35310574ab3452bc64613801f9406cf715cb
GaoningGao <33690279+GaoningGao@users.noreply.github.com>
2017-12-08 14:39:39 +0100

    Update mapgen.c

--- mapgen.c
+++ mapgen.c
@@ -1,1 +1,29 @@
-#include "mapgen.h"
+//
+//  display.c
+//  board
+//
+//  Created by app on 2017/11/21.
+//  Copyright © 2017年 ning. All rights reserved.
+//
+
+#include "display.h"
+#define SIZE 20
+
+int random_number(int min, int max){
+    return (rand()%(max-min+1)+min);
+}
+
+void print_map(char data[100][100], int rows, int columns,int min, int max){
+    
+    for(int i=0;i<columns;i++){
+        for(int j=0;j<rows;j++){
+            data[i][j]=random_number(min,max);
+            if(data[i][j]==0)
+                printf("   ");
+            else printf(" %d ",data[i][j]);
+            
+        }
+        printf("\n");
+        }
+    
+}
